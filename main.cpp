2d94fa86301fd7ca75824f15a07dc9613a6233fb
avo2003 <avetis.f.y@tumo.org>
2019-06-26 22:46:39 +0400

    learning c plus plus

---
+++ main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  /*  string name ="avo";
+    int age =16;
+
+    cout << "im anunes "<< name << " ye"<< endl;
+    cout << "yes "<< age <<" dareganem" << endl;
+    name= "nareg";
+    cout << "im pokhetsi"<< name << endl;
+    */
+
+   /* char grade ='A';
+    string phrase ="inch guze togh ella";
+    int age =88;
+    double gpa =2.5;
+    bool isMale =false;
+
+    cout <<false<<endl;
+    */
+   /* string phrase ="avon khelatsiye yev ashkharhi amena tuz dghane";
+    cout << phrase.length(); */
+
+
+
+/*  string phrase ="ammaaaa";
+    phrase[0]='b';
+    phrase[1]='a';
+    phrase[3]='y';
+    cout << phrase;
+    */
+
+   /* string text ="baron avo fishenkjian";
+    cout << text.find("ian",0) ;
+    */
+
+  /*  string text ="baron avo fishenkjian";
+    cout << text.substr(6,3) ;   */
+
+
+    /*    cout << 7 + 8 << endl;
+        cout << 7 * 8 << endl;
+        cout << 9 / 8 << endl;
+        cout << 7 % 3 << endl;   */
+
+
+    /*   int num=5;
+        num+=5;
+        num ++;
+        cout <<num; */
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
